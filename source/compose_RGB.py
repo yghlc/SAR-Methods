0cc5b874c2d29ea0e6cbd22790888c3df0999c13
Lingcao Huang <huanglingcao@gmail.com>
2022-03-09 19:11:37 -0700

    debug band order for gdal

--- source/compose_RGB.py
+++ source/compose_RGB.py
@@ -39,16 +39,29 @@ def construct_rgb_composite(VV_img, VH_img):
     # ratio_band = np.where(VH_band == 0., 0., ratio_band)
     ratio = Image(None)
     ratio.band = ratio_band
+    print('\n before quantile_clip')
+    print('VH,max, min, mean', np.max(VH.band), np.min(VH.band), np.mean(VH.band))
+    print('VV,max, min, mean', np.max(VV.band), np.min(VV.band), np.mean(VV.band))
+    print('ratio,max, min, mean', np.max(ratio.band), np.min(ratio.band), np.mean(ratio.band))
 
     # quantile clipping
+    print('\n after quantile_clip')
     VH.quantile_clip(upper_quantile=0.99)
     VV.quantile_clip(upper_quantile=0.99)
     ratio.quantile_clip(upper_quantile=0.99)
+    print('VH,max, min, mean', np.max(VH.band), np.min(VH.band), np.mean(VH.band))
+    print('VV,max, min, mean', np.max(VV.band), np.min(VV.band), np.mean(VV.band))
+    print('ratio,max, min, mean', np.max(ratio.band), np.min(ratio.band), np.mean(ratio.band))
 
     # map to interval [0,1]
     VH.map_to_interval(0, 1)
     VV.map_to_interval(0, 1)
     ratio.map_to_interval(0, 1)
+
+    print('\n after map_to_interval')
+    print('VH,max, min, mean', np.max(VH.band), np.min(VH.band), np.mean(VH.band))
+    print('VV,max, min, mean', np.max(VV.band), np.min(VV.band), np.mean(VV.band))
+    print('ratio,max, min, mean', np.max(ratio.band), np.min(ratio.band), np.mean(ratio.band))
 
     # return a numpy array because Image objects are only tested for single channel images
     rgb = np.zeros((ratio.band.shape[0], ratio.band.shape[1], 3))
@@ -68,6 +81,8 @@ def main():
     data_dir=os.path.expanduser('~/Bhaltos2/lingcaoHuang/flooding_area/Houston/compose_3bands_VH_VV_VHVV/RGB_VH_VV_data')
     VH_path = os.path.join(data_dir,'20170829_VH.tif')
     VV_path = os.path.join(data_dir,'20170829_VV.tif')
+    # VH_path = os.path.join(data_dir,'20170829_VH_0.00089831528412.tif')
+    # VV_path = os.path.join(data_dir,'20170829_VV_0.00089831528412.tif')
 
     VH_img = Image(VH_path)
     VV_img = Image(VV_path)
@@ -78,9 +93,12 @@ def main():
     # cluster = Cluster(geographic_bounds)
 
     rgb = construct_rgb_composite(VV_img, VH_img)
-
+    print('rgb.shape',rgb.shape)
+    rgb = rgb.transpose((2,0,1))        # reshape to band, height, width for GDAL
+    print('after transpose rgb.shape', rgb.shape)
+    rgb = rgb.astype(np.float32)
     # save to disk
-    RGB_path = os.path.join(data_dir,'20170829_RGB_composite_v3.tif')
+    RGB_path = os.path.join(data_dir,'20170829_RGB_composite_v3_100.tif')
     # RGB_img = Image(RGB_path)
     raster_io.save_numpy_array_to_rasterfile(rgb,RGB_path,VH_path)
 
