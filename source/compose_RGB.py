0c3a5b1029d9899c6539c56421fb1ae4aacd7204
Lingcao Huang <huanglingcao@gmail.com>
2022-03-13 21:22:06 -0600

    sync to RGB_fix

--- source/compose_RGB.py
+++ source/compose_RGB.py
@@ -72,36 +72,80 @@ def construct_rgb_composite(VV_img, VH_img):
 
     return rgb
 
-def main():
-    # merge the two frames first:
-    # gdal_merge.py -n 0 -a_nodata 0  -o 20170829_VH.tif */*VH*.tif
-    # gdal_merge.py -n 0 -a_nodata 0  -o 20170829_VV.tif */*VV*.tif
-
-    # require a lot of memory, run on tesia
-    data_dir=os.path.expanduser('~/Data/flooding_area/Houston/compose_3bands_VH_VV_VHVV/RGB_VH_VV_data')
-    # data_dir=os.path.expanduser('~/Bhaltos2/lingcaoHuang/flooding_area/Houston/compose_3bands_VH_VV_VHVV/RGB_VH_VV_data')
-    # VH_path = os.path.join(data_dir,'20170829_VH.tif')
-    # VV_path = os.path.join(data_dir,'20170829_VV.tif')
-    VH_path = os.path.join(data_dir,'20170829_VH_0.00089831528412.tif')
-    VV_path = os.path.join(data_dir,'20170829_VV_0.00089831528412.tif')
+def compose_RGB_conor(VH_path, VV_path):
 
     VH_img = Image(VH_path)
     VV_img = Image(VV_path)
     VH_img.read()
     VV_img.read()
 
-    # geographic_bounds = [-118.7, -117.7, 33.5, 34.2]
-    # cluster = Cluster(geographic_bounds)
+    geographic_bounds = [-97.6256427, -94.3827246, 28.0349412, 31.4594987]
+    cluster = Cluster(geographic_bounds)
 
-    rgb = construct_rgb_composite(VV_img, VH_img)
-    print('rgb.shape',rgb.shape)
-    rgb = rgb.transpose((2,0,1))        # reshape to band, height, width for GDAL
-    print('after transpose rgb.shape', rgb.shape)
-    rgb = rgb.astype(np.float32)
-    # save to disk
-    RGB_path = os.path.join(data_dir,'20170829_RGB_composite_v3.tif')
-    # RGB_img = Image(RGB_path)
-    raster_io.save_numpy_array_to_rasterfile(rgb,RGB_path,VH_path)
+    # changed image order to be how it should be in Cluster.py
+    rgb = cluster.construct_rgb_composite(VV_img, VH_img)
+
+    # this code adds a nodata value
+
+    bad = rgb[0, 0, :]
+    test = np.full(rgb.shape, rgb[0, 0, :])
+    test2 = np.sum(rgb - test, axis=2)
+    ys, xs = np.where(test2 == 0)
+    rgbnew = rgb.copy()
+    rgbnew[ys, xs] = 999
+
+    import rasterio
+    from rasterio.enums import ColorInterp
+    # tif_path =  "/home/conor/Research/kms/RGB_Final2.tif"
+    tif_path =  os.path.join(os.path.dirname(VH_path), 'RGB_Final2.tif')
+    tif_kwargs = VV_img.raster.meta.copy()
+    tif_kwargs.update({
+        'nodata': 999,
+        'dtype': rgbnew.dtype,
+        'count': 3
+    })
+    new_tif = rasterio.open(tif_path, 'w', **tif_kwargs)
+    new_tif.colorinterp = [ColorInterp.red, ColorInterp.green, ColorInterp.blue]
+    new_tif.write(rgbnew[:, :, 0], indexes=1)
+    new_tif.write(rgbnew[:, :, 1], indexes=2)
+    new_tif.write(rgbnew[:, :, 2], indexes=3)
+    new_tif.close()
+
+
+
+def main():
+    # merge the two frames first:
+    # gdal_merge.py -n 0 -a_nodata 0  -o 20170829_VH.tif */*VH*.tif
+    # gdal_merge.py -n 0 -a_nodata 0  -o 20170829_VV.tif */*VV*.tif
+
+    # require a lot of memory, run on tesia
+    # data_dir=os.path.expanduser('~/Data/flooding_area/Houston/compose_3bands_VH_VV_VHVV/RGB_VH_VV_data')
+    data_dir=os.path.expanduser('~/Bhaltos2/lingcaoHuang/flooding_area/Houston/compose_3bands_VH_VV_VHVV/RGB_VH_VV_data')
+    VH_path = os.path.join(data_dir,'20170829_VH.tif')
+    VV_path = os.path.join(data_dir,'20170829_VV.tif')
+    # VH_path = os.path.join(data_dir,'20170829_VH_0.00089831528412.tif')
+    # VV_path = os.path.join(data_dir,'20170829_VV_0.00089831528412.tif')
+
+    compose_RGB_conor(VH_path,VV_path)
+
+
+    # VH_img = Image(VH_path)
+    # VV_img = Image(VV_path)
+    # VH_img.read()
+    # VV_img.read()
+    #
+    # # geographic_bounds = [-118.7, -117.7, 33.5, 34.2]
+    # # cluster = Cluster(geographic_bounds)
+    #
+    # rgb = construct_rgb_composite(VV_img, VH_img)
+    # print('rgb.shape',rgb.shape)
+    # rgb = rgb.transpose((2,0,1))        # reshape to band, height, width for GDAL
+    # print('after transpose rgb.shape', rgb.shape)
+    # rgb = rgb.astype(np.float32)
+    # # save to disk
+    # RGB_path = os.path.join(data_dir,'20170829_RGB_composite_v3.tif')
+    # # RGB_img = Image(RGB_path)
+    # raster_io.save_numpy_array_to_rasterfile(rgb,RGB_path,VH_path)
 
 
 if __name__ == '__main__':
