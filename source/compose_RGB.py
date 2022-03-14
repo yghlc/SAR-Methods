e10e3ae0ced19934717c731f7dbb22c24b01c217
Lingcao Huang <huanglingcao@gmail.com>
2022-03-13 21:05:20 -0600

    RGB_fix

--- source/compose_RGB.py
+++ source/compose_RGB.py
@@ -33,35 +33,36 @@ def construct_rgb_composite(VV_img, VH_img):
     VH.convert_to_dB()
     VV.convert_to_dB()
 
+    print('\n before quantile_clip')
+    print('VH,max, min, mean', np.max(VH.band), np.min(VH.band), np.mean(VH.band))
+    print('VV,max, min, mean', np.max(VV.band), np.min(VV.band), np.mean(VV.band))
+    # print('ratio,max, min, mean', np.max(ratio.band), np.min(ratio.band), np.mean(ratio.band))
+
+    # quantile clipping
+    print('\n after quantile_clip')
+    VH.quantile_clip(upper_quantile=0.99)
+    VV.quantile_clip(upper_quantile=0.99)
+
+    print('VH,max, min, mean', np.max(VH.band), np.min(VH.band), np.mean(VH.band))
+    print('VV,max, min, mean', np.max(VV.band), np.min(VV.band), np.mean(VV.band))
+    # print('ratio,max, min, mean', np.max(ratio.band), np.min(ratio.band), np.mean(ratio.band))
+
+    # map to interval [0,1]
+    VH.map_to_interval(0, 1)
+    VV.map_to_interval(0, 1)
+
+    print('\n after map_to_interval')
+    print('VH,max, min, mean', np.max(VH.band), np.min(VH.band), np.mean(VH.band))
+    print('VV,max, min, mean', np.max(VV.band), np.min(VV.band), np.mean(VV.band))
+    # print('ratio,max, min, mean', np.max(ratio.band), np.min(ratio.band), np.mean(ratio.band))
+
     # make the ratio band
     ratio_band = np.divide(VH.band, VV.band)
     ratio_band = np.where(VV.band == 0., 0., ratio_band)  # handle divide by zero
     # ratio_band = np.where(VH_band == 0., 0., ratio_band)
     ratio = Image(None)
     ratio.band = ratio_band
-    print('\n before quantile_clip')
-    print('VH,max, min, mean', np.max(VH.band), np.min(VH.band), np.mean(VH.band))
-    print('VV,max, min, mean', np.max(VV.band), np.min(VV.band), np.mean(VV.band))
-    print('ratio,max, min, mean', np.max(ratio.band), np.min(ratio.band), np.mean(ratio.band))
-
-    # quantile clipping
-    print('\n after quantile_clip')
-    VH.quantile_clip(upper_quantile=0.99)
-    VV.quantile_clip(upper_quantile=0.99)
-    ratio.quantile_clip(upper_quantile=0.99)
-    print('VH,max, min, mean', np.max(VH.band), np.min(VH.band), np.mean(VH.band))
-    print('VV,max, min, mean', np.max(VV.band), np.min(VV.band), np.mean(VV.band))
-    print('ratio,max, min, mean', np.max(ratio.band), np.min(ratio.band), np.mean(ratio.band))
-
-    # map to interval [0,1]
-    VH.map_to_interval(0, 1)
-    VV.map_to_interval(0, 1)
     ratio.map_to_interval(0, 1)
-
-    print('\n after map_to_interval')
-    print('VH,max, min, mean', np.max(VH.band), np.min(VH.band), np.mean(VH.band))
-    print('VV,max, min, mean', np.max(VV.band), np.min(VV.band), np.mean(VV.band))
-    print('ratio,max, min, mean', np.max(ratio.band), np.min(ratio.band), np.mean(ratio.band))
 
     # return a numpy array because Image objects are only tested for single channel images
     rgb = np.zeros((ratio.band.shape[0], ratio.band.shape[1], 3))
@@ -77,12 +78,12 @@ def main():
     # gdal_merge.py -n 0 -a_nodata 0  -o 20170829_VV.tif */*VV*.tif
 
     # require a lot of memory, run on tesia
-    # data_dir=os.path.expanduser('~/Data/flooding_area/Houston/compose_3bands_VH_VV_VHVV/RGB_VH_VV_data')
-    data_dir=os.path.expanduser('~/Bhaltos2/lingcaoHuang/flooding_area/Houston/compose_3bands_VH_VV_VHVV/RGB_VH_VV_data')
-    VH_path = os.path.join(data_dir,'20170829_VH.tif')
-    VV_path = os.path.join(data_dir,'20170829_VV.tif')
-    # VH_path = os.path.join(data_dir,'20170829_VH_0.00089831528412.tif')
-    # VV_path = os.path.join(data_dir,'20170829_VV_0.00089831528412.tif')
+    data_dir=os.path.expanduser('~/Data/flooding_area/Houston/compose_3bands_VH_VV_VHVV/RGB_VH_VV_data')
+    # data_dir=os.path.expanduser('~/Bhaltos2/lingcaoHuang/flooding_area/Houston/compose_3bands_VH_VV_VHVV/RGB_VH_VV_data')
+    # VH_path = os.path.join(data_dir,'20170829_VH.tif')
+    # VV_path = os.path.join(data_dir,'20170829_VV.tif')
+    VH_path = os.path.join(data_dir,'20170829_VH_0.00089831528412.tif')
+    VV_path = os.path.join(data_dir,'20170829_VV_0.00089831528412.tif')
 
     VH_img = Image(VH_path)
     VV_img = Image(VV_path)
